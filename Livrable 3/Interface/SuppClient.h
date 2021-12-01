0bc91d9d4bddf039e28b9d1bbb665f4d2c5808da
Corentin VALET <cocotereg@gmail.com>
2021-12-01 16:26:42 +0100

    Ajout barre mouv + couleur suppclient

--- Livrable 3/Interface/SuppClient.h
+++ Livrable 3/Interface/SuppClient.h
@@ -23,6 +23,9 @@ namespace Interface {
 			//
 		}
 
+	private:
+		Point PanelMouseDownLocation;
+
 	protected:
 		/// <summary>
 		/// Nettoyage des ressources utilisées.
@@ -94,6 +97,8 @@ namespace Interface {
 			this->panelmove1->Name = L"panelmove1";
 			this->panelmove1->Size = System::Drawing::Size(1057, 39);
 			this->panelmove1->TabIndex = 14;
+			this->panelmove1->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &SuppClient::panelmove1_MouseDown);
+			this->panelmove1->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &SuppClient::panelmove1_MouseMove);
 			// 
 			// X
 			// 
@@ -108,11 +113,14 @@ namespace Interface {
 			this->X->TabIndex = 1;
 			this->X->Text = L"X";
 			this->X->UseVisualStyleBackColor = false;
+			this->X->Click += gcnew System::EventHandler(this, &SuppClient::X_Click);
+			this->X->MouseEnter += gcnew System::EventHandler(this, &SuppClient::X_MouseEnter);
+			this->X->MouseLeave += gcnew System::EventHandler(this, &SuppClient::X_MouseLeave);
 			// 
 			// dataGridView1
 			// 
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(74, 195);
+			this->dataGridView1->Location = System::Drawing::Point(74, 206);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->RowHeadersWidth = 51;
 			this->dataGridView1->RowTemplate->Height = 24;
@@ -121,21 +129,21 @@ namespace Interface {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(176, 150);
+			this->textBox1->Location = System::Drawing::Point(176, 157);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(126, 22);
 			this->textBox1->TabIndex = 16;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(176, 99);
+			this->textBox2->Location = System::Drawing::Point(176, 106);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(126, 22);
 			this->textBox2->TabIndex = 17;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(732, 122);
+			this->textBox3->Location = System::Drawing::Point(732, 129);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(100, 22);
 			this->textBox3->TabIndex = 18;
@@ -147,7 +155,7 @@ namespace Interface {
 				static_cast<System::Byte>(0)));
 			this->labelnclient->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(154)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(152)));
-			this->labelnclient->Location = System::Drawing::Point(621, 116);
+			this->labelnclient->Location = System::Drawing::Point(621, 123);
 			this->labelnclient->Name = L"labelnclient";
 			this->labelnclient->Size = System::Drawing::Size(104, 32);
 			this->labelnclient->TabIndex = 19;
@@ -160,7 +168,7 @@ namespace Interface {
 				static_cast<System::Byte>(0)));
 			this->prénom->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(154)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(152)));
-			this->prénom->Location = System::Drawing::Point(68, 90);
+			this->prénom->Location = System::Drawing::Point(68, 97);
 			this->prénom->Name = L"prénom";
 			this->prénom->Size = System::Drawing::Size(98, 32);
 			this->prénom->TabIndex = 20;
@@ -173,7 +181,7 @@ namespace Interface {
 				static_cast<System::Byte>(0)));
 			this->nom->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(154)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(152)));
-			this->nom->Location = System::Drawing::Point(68, 140);
+			this->nom->Location = System::Drawing::Point(68, 147);
 			this->nom->Name = L"nom";
 			this->nom->Size = System::Drawing::Size(68, 32);
 			this->nom->TabIndex = 21;
@@ -186,7 +194,7 @@ namespace Interface {
 				static_cast<System::Byte>(0)));
 			this->labelsupprimer->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(154)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(152)));
-			this->labelsupprimer->Location = System::Drawing::Point(677, 43);
+			this->labelsupprimer->Location = System::Drawing::Point(677, 50);
 			this->labelsupprimer->Name = L"labelsupprimer";
 			this->labelsupprimer->Size = System::Drawing::Size(155, 32);
 			this->labelsupprimer->TabIndex = 22;
@@ -200,7 +208,7 @@ namespace Interface {
 				static_cast<System::Byte>(0)));
 			this->buttonrechercher->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(154)),
 				static_cast<System::Int32>(static_cast<System::Byte>(140)), static_cast<System::Int32>(static_cast<System::Byte>(152)));
-			this->buttonrechercher->Location = System::Drawing::Point(336, 112);
+			this->buttonrechercher->Location = System::Drawing::Point(336, 119);
 			this->buttonrechercher->Name = L"buttonrechercher";
 			this->buttonrechercher->Size = System::Drawing::Size(169, 50);
 			this->buttonrechercher->TabIndex = 23;
@@ -214,7 +222,7 @@ namespace Interface {
 				static_cast<System::Byte>(0)));
 			this->buttonsupprimer->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(154)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(152)));
-			this->buttonsupprimer->Location = System::Drawing::Point(876, 103);
+			this->buttonsupprimer->Location = System::Drawing::Point(876, 110);
 			this->buttonsupprimer->Name = L"buttonsupprimer";
 			this->buttonsupprimer->Size = System::Drawing::Size(169, 50);
 			this->buttonsupprimer->TabIndex = 24;
@@ -228,7 +236,7 @@ namespace Interface {
 				static_cast<System::Byte>(0)));
 			this->labelrecherche->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(154)), static_cast<System::Int32>(static_cast<System::Byte>(140)),
 				static_cast<System::Int32>(static_cast<System::Byte>(152)));
-			this->labelrecherche->Location = System::Drawing::Point(147, 43);
+			this->labelrecherche->Location = System::Drawing::Point(147, 50);
 			this->labelrecherche->Name = L"labelrecherche";
 			this->labelrecherche->Size = System::Drawing::Size(131, 32);
 			this->labelrecherche->TabIndex = 25;
@@ -268,5 +276,39 @@ namespace Interface {
 	}
 private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
 }
+private: System::Void X_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Close();
+}
+private: System::Void X_MouseEnter(System::Object^ sender, System::EventArgs^ e) {
+	this->X->BackColor = Color::FromArgb(221, 25, 23);
+}
+private: System::Void X_MouseLeave(System::Object^ sender, System::EventArgs^ e) {
+	this->X->BackColor = Color::FromArgb(255, 255, 255);
+}
+	   //Drag and move / panelmove1
+
+	private: void dragFormMouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+	{
+		if (e->Button == System::Windows::Forms::MouseButtons::Left)
+			PanelMouseDownLocation = e->Location;
+	}
+	private: void dragFormMouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+	{
+		if (e->Button == System::Windows::Forms::MouseButtons::Left)
+		{
+			SuppClient::Left += e->X - PanelMouseDownLocation.X;
+			SuppClient::Top += e->Y - PanelMouseDownLocation.Y;
+		}
+	}
+
+	private: System::Void panelmove1_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+	{
+		dragFormMouseDown(sender, e);
+	}
+	private: System::Void panelmove1_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+	{
+		dragFormMouseMove(sender, e);
+	}
+
 };
 }
